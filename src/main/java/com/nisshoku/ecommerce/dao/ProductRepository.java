21674e47ef22c9360aa97de05e87d08e82bd0ee3
Taka-kun <uchiha.takahiro@gmail.com>
2020-08-15 20:43:33 +0200

    Added Search By Category Endpoint

--- src/main/java/com/nisshoku/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/nisshoku/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,14 @@
 package com.nisshoku.ecommerce.dao;
 
 import com.nisshoku.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin()
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    Page<Product> findByCategoryId(@RequestParam Long id, Pageable pageable);
 }
